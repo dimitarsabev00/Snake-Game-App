104b4929f1cb5876b5ec7bd0d05035593424f203
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-14 13:33:36 +0200

    Added PlayArea Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import "./App.scss";
+import PlayArea from "./PlayArea/PlayArea";
 
 const App = () => {
-  return <div className="container-app"></div>;
+  return (
+    <div className="container-app">
+      <PlayArea />
+    </div>
+  );
 };
 
 export default App;
